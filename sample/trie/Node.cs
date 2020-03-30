dc620b05502ebc78eeb51db9d3ff78c76768f08d
Vikram <vikramdh@microsoft.com>
2020-03-29 22:46:53 -0700

    created basic Node class with only value and children properties.

---
+++ sample/trie/Node.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace trie
+{
+    public class Node
+    {
+        public char Value { get; set; }
+        public List<Node> Children { get; set; }
+
+        public Node(char value)
+        {
+            Value = value;
+            Children = new List<Node>();
+        }
+    }
+}
